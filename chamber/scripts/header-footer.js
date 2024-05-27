7b7a81adf7aeb2f7f23df9a6611d8865a400ea7f
abeHennum <hennumabraham@gmail.com>
2024-05-27 17:47:24 -0600

    chamber-updates

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -3,3 +3,10 @@ datespan.innerText = new Date().getFullYear()
 
 let lastmodifiedspan = document.querySelector("#lastModified")
 lastmodifiedspan.innerText = document.lastModified
+
+
+//dark mode stuff
+let darkbutton = document.getElementById("dark-mode")
+darkbutton.addEventListener("click", () => {
+    
+})
