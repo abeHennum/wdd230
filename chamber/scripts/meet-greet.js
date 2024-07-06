1a66f0e860c78dbeb46451ef2c4372dd0baa04d1
abeHennum <hennumabraham@gmail.com>
2024-07-06 11:48:59 -0600

    join chamber button

--- chamber/scripts/meet-greet.js
+++ chamber/scripts/meet-greet.js
@@ -8,3 +8,10 @@ if (dayofweek >= 1 && dayofweek <= 3){
 document.querySelector(".meet-greet button").addEventListener("click", () => {
     document.querySelector(".meet-greet").classList.remove("show")
 })
+
+
+function openJoinPage(){
+    window.open("join.html")
+}
+
+document.querySelector(".hero button").addEventListener("click", openJoinPage)
